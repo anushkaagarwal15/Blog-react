bebcc398207ac6f02449408cc6cb56547ca54d6b
anushkaagarwal15 <raanushka15@gmail.com>
2024-02-26 00:38:49 +0530

    added new functions

--- api/index.js
+++ api/index.js
@@ -5,12 +5,11 @@ const mongoose = require("mongoose");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONOGO_URL, {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
 })
-.then(console.log("Connected to MongoDB"))
+.then(() => console.log("Connected to MongoDB"))
 .catch((err) => console.log(err));
 
 
